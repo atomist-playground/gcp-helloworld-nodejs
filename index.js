c6200df2e88706b4e33d6bcf28e17325162db18e
Christian Dupuis <cd@atomist.com>
2020-06-25 23:10:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   console.log("Hello world received a request.");
 
   const target = process.env.TARGET || "World";
-  res.send(`Hello ${target}!`);
+  res.send(`Hello ${target}, from cloud run!`);
 });
 
 const port = process.env.PORT || 8080;
