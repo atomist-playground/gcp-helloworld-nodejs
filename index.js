d54180ff91bddedfef84c8efa21780fb075973fc
Christian Dupuis <cd@atomist.com>
2020-06-29 15:17:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  const target = process.env.TARGET || "World";
+  const target = process.env.TARGET || 'Accenture';
   res.send(`Hello ${target}!`);
 });
 
