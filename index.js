d7fc12cc6691cba0b54dd5091a84dd6fd7f30aef
Christian Dupuis <cd@atomist.com>
2020-06-29 13:34:54 +0200

    Remove console.log statements

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Hello world received a request.");
-
   const target = process.env.TARGET || "World";
   res.send(`Hello ${target}!`);
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log("Hello world listening on port", port);
-});
+app.listen(port);
